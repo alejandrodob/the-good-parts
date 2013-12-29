cd4ea65bc7c5862f9e5ca90787c95d091a66235f
alejandrodob <alejandrodob@gmail.com>
2013-12-29 14:04:42 +0100

    added more tests to object

--- test/spec/spec.js
+++ test/spec/spec.js
@@ -4,15 +4,34 @@ describe("The javascript languaje and its good and bad parts", function() {
 
   describe("Objects", function() {
 
-    it("inherit attributes from the prototype of the function that created them", function() {
-        function CreateObject() {}
-        CreateObject.prototype.attributeOne = 'attribute';
-        var obj = new CreateObject();
-        var inheritedAttribute = obj.attributeOne;
-
-        expect(inheritedAttribute).toEqual('attribute');
+    it("are created with a link to Object.prototype, if created as literals", function() {
+      var object = {};
+      expect(Object.getPrototypeOf(object)).toEqual(Object.prototype);
     });
 
+    it("are created with a link to the prototype of the function that created them", function() {
+      function Creator() {}
+      var prototype = {method: function() {}};
+      Creator.prototype = prototype;
+      var obj = new Creator();
+
+      expect(Object.getPrototypeOf(obj)).toEqual(prototype);
+    });
+
+    it("inherit attributes from the prototype of the function that created them", function() {
+      function CreateObject() {}
+      CreateObject.prototype.attributeOne = 'attribute';
+      var obj = new CreateObject();
+      var inheritedAttribute = obj.attributeOne;
+
+      expect(inheritedAttribute).toEqual('attribute');
+    });
+
+  });
+
+  describe("Functions", function() {
+
+    it();
   });
 
 
