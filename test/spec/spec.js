8f24cb1d9340ba42af2c424b70a810619330464b
alejandrodob <alejandrodob@gmail.com>
2013-12-29 13:07:53 +0100

    added object test

--- test/spec/spec.js
+++ test/spec/spec.js
@@ -1,14 +1,19 @@
 context = describe;
 
-describe("The javascript object inheritance types", function() {
+describe("The javascript languaje and its good and bad parts", function() {
 
-    describe("the pseudoclassical", function() {
+  describe("Objects", function() {
 
-        it("", function() {
-            
-        });
+    it("inherit attributes from the prototype of the function that created them", function() {
+        function CreateObject() {}
+        CreateObject.prototype.attributeOne = 'attribute';
+        var obj = new CreateObject();
+        var inheritedAttribute = obj.attributeOne;
 
+        expect(inheritedAttribute).toEqual('attribute');
     });
+
+  });
 
 
 });
